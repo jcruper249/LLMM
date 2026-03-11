0b6b775e17379884fd53badf2573c153ace31b8c
jcruper249 <jcruper249@g.educaand.es>
2026-03-11 23:44:19 +0100

    k

--- JAVA SCRIPT/Prueba arrays/js.js
+++ JAVA SCRIPT/Prueba arrays/js.js
@@ -1,5 +1,13 @@
 const lista = ["persona", "perro", "gato", "casa", "coche"];
 function listaa() {
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+let text = "<ul>";
+for (let i = 0; i < fruits.length; i++) {
+  text += "<li>" + fruits[i] + "</li>";
+}
+text += "</ul>";
+document.getElementById("lista1").innerHTML = text;
 for (let i = 0; i < lista.length; i++) {
     document.getElementsByTagName("p")[i].innerHTML = lista[1] +[i];
 }
